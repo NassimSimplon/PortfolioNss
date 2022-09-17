7cd7bb53c76cee554c2a6bcaf23277090c2ab8f0
NassimSimplon <nassimkhlifisimplonenda@gmail.com>
2022-09-17 17:21:58 +0100

    Create Navbar

---
+++ src/Components/HomeSection/HomeSection.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import './HomeSection.css'
+import {Constants} from '../../Utils/ImportFile'
+import Nav from '../../Shared/Navbar/Nav'
+ 
+const HomeSection = () => {
+  
+  return (
+    <div id='Home'>
+      <Nav />
+    <div className='empty-space'>
+        
+    </div>
+      <div className='content'>
+        <h1>dasdasd</h1>
+        <h1>khlifi</h1>
+  
+
+      </div>
+    </div>
+  )
+}
+
+export default HomeSection
